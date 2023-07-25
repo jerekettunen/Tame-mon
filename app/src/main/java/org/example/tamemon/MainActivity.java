2896ad14f76e876b2e819a60214b5d2dc95fcfd6
jekettun <jere.kettunen@student.oulu.fi>
2023-07-25 07:48:42 +0300

    Start screen structure update

--- app/src/main/java/org/example/tamemon/MainActivity.java
+++ app/src/main/java/org/example/tamemon/MainActivity.java
@@ -1,8 +1,15 @@
 package org.example.tamemon;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import org.example.tamemon.fragments.FragmentNewPlayer;
+import org.example.tamemon.fragments.InfoFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnContinue = findViewById(R.id.btnContinue);
+        Button btnNewPlay = findViewById(R.id.btnNewPlay);
+        Button info = findViewById(R.id.btnInfo);
+
+        btnContinue.setOnClickListener(listener);
+        btnNewPlay.setOnClickListener(listener);
+        info.setOnClickListener(listener);
     }
+
+
+    private View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Fragment fragment = null;
+            int id = view.getId();
+            if (id == R.id.btnNewPlay){
+                fragment = new FragmentNewPlayer();
+            } else if (id == R.id.btnInfo) {
+                fragment = new InfoFragment();
+            } else if (id == R.id.btnContinue) {
+                Intent intent = new Intent(view.getContext(), PlayActivity.class);
+                startActivity(intent);
+                return;
+            }
+            
+            getSupportFragmentManager().beginTransaction().setCustomAnimations(
+                    R.anim.slide_in,  // enter
+                    R.anim.fade_out,  // exit
+                    R.anim.fade_in,   // popEnter
+                    R.anim.slide_out  // popExit
+                    )
+                    .replace(R.id.frameStart, fragment)
+                    .commit();
+
+        }
+    };
 }
